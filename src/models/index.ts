1e9700070792625d536d6b79cdfd6f0eca062f36
Kevin Reyes <kreyesvelasquez@gmail.com>
2026-06-09 11:05:14 -0600

    Rename Size files to enhance project structure

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,7 +2,7 @@ export * from "./role-model";
 export * from "./user-model";
 export * from "./flavor-model";
 export * from "./topping-model";
-export * from "./sizeModel";
+export * from "./size-model";
 export * from "./pizzaModel";
 export * from "./statusModel";
 export * from "./orderModel";
--- src/services/index.ts
+++ src/services/index.ts
@@ -2,7 +2,7 @@ export * from "./role-service";
 export * from "./user-service";
 export * from "./flavor-service";
 export * from "./topping-service";
-export * from "./sizeService";
+export * from "./size-service";
 export * from "./pizzaService";
 export * from "./statusService";
 export * from "./orderService";
